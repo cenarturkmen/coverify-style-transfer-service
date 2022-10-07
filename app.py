9fcb711898ea38e0801e361932f8fb6c6fc28d59
Caner Türkmen <canerturkmen@msn.com>
2022-10-07 13:29:02 +0300

    initial

---
+++ app.py
@@ -0,0 +1,51 @@
+import io
+import json
+
+from torchvision import models
+import torchvision
+import torchvision.transforms as transforms
+from PIL import Image
+from PIL import __version__ as PILLOW_VERSION
+
+from flask import Flask, jsonify, request
+
+
+app = Flask(__name__)
+imagenet_class_index = json.load(open('./imagenet_class_index.json'))
+model = models.densenet121(weights=torchvision.models.DenseNet121_Weights.DEFAULT)
+model.eval()
+
+
+def transform_image(image_bytes):
+    my_transforms = transforms.Compose([transforms.Resize(255),
+                                        transforms.CenterCrop(224),
+                                        transforms.ToTensor(),
+                                        transforms.Normalize(
+                                            [0.485, 0.456, 0.406],
+                                            [0.229, 0.224, 0.225])])
+    image = Image.open(io.BytesIO(image_bytes))
+    return my_transforms(image).unsqueeze(0)
+
+
+def get_prediction(image_bytes):
+    tensor = transform_image(image_bytes=image_bytes)
+    outputs = model.forward(tensor)
+    _, y_hat = outputs.max(1)
+    predicted_idx = str(y_hat.item())
+    return imagenet_class_index[predicted_idx]
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    print("zortttttt", request.files)
+    if request.method == 'POST':
+        file = request.files['file']
+        img_bytes = file.read()
+        class_id, class_name = get_prediction(image_bytes=img_bytes)
+        print(class_name)
+        return jsonify({'class_id': class_id, 'class_name': class_name})
+
+
+if __name__ == '__main__':
+    app.run()
+
